877c6002f5363b999235aef8eb6fa72e49341772
Togira <70365614+Togira123@users.noreply.github.com>
2024-03-29 19:16:33 +0100

    made working animations

---
+++ scenes/camera_3d.gd
@@ -0,0 +1,14 @@
+extends Camera3D
+
+@onready var player = $"../Player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.z = player.position.z - 2
+	position.x = player.position.x
+	position.y = player.position.y + 2
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @onready var armature = $Armature
 @onready var animation_tree = $AnimationTree
-@onready var animation_state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+@onready var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
 
 @onready var level = $"../Level"
 
@@ -12,45 +12,38 @@ const LERP_VAL = 0.3
 const FALL_ACCELERATION = 75
 
 var has_spinned = false # makes sure players can only roll once after jumping
-var is_jumping = false
-var is_rolling = false
 
 func _physics_process(delta):
 	animation_tree.set("parameters/conditions/is_rolling", false)
 	animation_tree.set("parameters/conditions/is_jumping", false)
-	# Add the gravity
 	if not is_on_floor():
-		if not has_spinned and is_jumping and not is_rolling and Input.is_action_just_pressed("jump"):
-			print("has_spinned ", has_spinned)
-			print("is_jumping ", is_jumping)
-			print("is_rolling ", is_rolling)
-			print("stored_is_jumping ", animation_tree.get("parameters/conditions/is_jumping"))
-			print("stored_is_rolling ", animation_tree.get("parameters/conditions/is_rolling"))
+		if state_machine.get_current_node() == "roll":
+			# Drop quickly if player is rolling
+			velocity.y -= FALL_ACCELERATION * 3 * delta
+		elif not has_spinned and state_machine.get_current_node() == "jump_blend_tree" and Input.is_action_just_pressed("jump"):
 			animation_tree.set("parameters/conditions/is_spinning", true)
-			#animation_state_machine.travel("jump_blend_tree")
-			
 			has_spinned = true
 		
 		if animation_tree.get("parameters/conditions/is_spinning"):
+			rotation.x = lerp(rotation.x, PI / 2.0, LERP_VAL / 2.0)
 			velocity.y = 0.0
 		else:
+			if rotation.x != 0:
+				rotation.x = lerp(rotation.x, 0.0, LERP_VAL / 2.0)
+			# Gravity
 			velocity.y -= FALL_ACCELERATION * delta
 	else:
 		has_spinned = false
-		is_jumping = false
+		rotation.x = 0
 	# 	Handle jump
 		if Input.is_action_just_pressed("jump"):
 			animation_tree.set("parameters/conditions/is_jumping", true)
-			is_jumping = true
 			velocity.y = JUMP_VELOCITY
 
 	# Handle roll
 	if Input.is_action_just_pressed("roll"):
 		animation_tree.set("parameters/conditions/is_rolling", true)
-		is_rolling = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Vector3.ZERO
 	direction.z += 1
 	# Get inputs
@@ -75,9 +68,9 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-
 func _on_hitbox_area_entered(area):
 	print("Hit letter!") # Replace with function body.
+
 
 
 func _on_animation_tree_animation_started(anim_name):
@@ -87,7 +80,3 @@ func _on_animation_tree_animation_finished(anim_name):
 	print(anim_name)
 	if anim_name == "spin":
 		animation_tree.set("parameters/conditions/is_spinning", false)
-	elif anim_name == "jump_blend_tree":
-		is_jumping = false
-	elif anim_name == "roll":
-		is_rolling = false
