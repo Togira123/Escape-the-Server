9a539353ba725716a39c2f88df442a3301ba5c2e
Togira <70365614+Togira123@users.noreply.github.com>
2024-04-01 20:17:12 +0200

    cleaned up

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -2,11 +2,6 @@ extends Camera3D
 
 @onready var player = $"../Player"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.z = player.position.z - 2
