5e3fe373ec6deb96bc15c87eb26ac2fcc01e1ff3
Togira <70365614+Togira123@users.noreply.github.com>
2024-04-17 09:00:18 +0200

    renamed variable

--- scripts/laser_pivot.gd
+++ scripts/laser_pivot.gd
@@ -2,6 +2,6 @@ extends Node3D
 
 @onready var laser = $Laser
 
-func scale_to(scale: float):
-	var scale_vec = Vector3(scale, scale, scale)
+func scale_to(new_scale: float):
+	var scale_vec = Vector3(new_scale, new_scale, new_scale)
 	laser.scale = scale_vec
