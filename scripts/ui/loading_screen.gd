685482e1f6b6cf4a5eff8850731613e67f007d49
Togira <70365614+Togira123@users.noreply.github.com>
2024-04-27 00:16:36 +0200

    also show tunnel during loading screen for shader compilation

--- scripts/ui/loading_screen.gd
+++ scripts/ui/loading_screen.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var player_camera = $"../PlayerCamera"
+@onready var TUNNEL = preload("res://scenes/tunnel.tscn")
 
 var moved = false
 var old_pos: Vector3
@@ -17,6 +18,9 @@ func _process(delta):
 		player_camera.position.z = -2
 		player_camera.position.x = 0
 		player_camera.rotation.x = -13.8 / 180 * PI
+		var tunnel = TUNNEL.instantiate()
+		tunnel.position.z = 50
+		add_child(tunnel)
 		moved = true
 	else:
 		player_camera.position = old_pos
