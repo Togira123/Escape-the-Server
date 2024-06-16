92c2980619fe7e2786f1c54eefe3812afeecfec7
Togira <70365614+Togira123@users.noreply.github.com>
2024-06-16 13:30:34 +0200

    building indicator turns red if player cannot build

--- scripts/building/building_indicator.gd
+++ scripts/building/building_indicator.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var level = $"../../Level"
 @onready var player = $"../../Player"
+@onready var cube = $"Cube"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +14,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	position.x = ceil(player.position.x * 5 / 100) * 20 - 10
 	position.z = ceil(player.position.z / 40) * 40 + 20
-	print(position.z)
+	var needed_ind = int(ceil(player.position.z / 40) * 2 + 1) % level.LOADED_MODULES_SIZE
+	var module = level.loaded_modules[needed_ind]
+	if module.built_at.has(int(position.x)) or player.material_count < 10:
+		cube.mesh.surface_get_material(0).set_shader_parameter("color", Color("ff0000"))
+	else:
+		cube.mesh.surface_get_material(0).set_shader_parameter("color", Color("00ffff"))
